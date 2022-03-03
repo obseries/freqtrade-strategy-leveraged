c6f748c81e556aab1286e92cc4106219ec0a3c9c
Luca Forni <l.forni@modalsource.com>
2022-03-03 16:43:51 +0100

    macd simple strategy

--- Leveraged.py
+++ Leveraged.py
@@ -1,216 +1,116 @@
-# --- Do not remove these libs ---
-from freqtrade.strategy.interface import IStrategy
+from freqtrade.strategy import IStrategy, merge_informative_pair
 from typing import Dict, List
-from functools import reduce
 from pandas import DataFrame
-# --------------------------------
-
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
-from typing import Dict, List
-from functools import reduce
-from pandas import DataFrame, DatetimeIndex, merge
-# --------------------------------
-
-import talib.abstract as ta
-import freqtrade.vendor.qtpylib.indicators as qtpylib
-import numpy  # noqa
+from freqtrade.exchange import timeframe_to_minutes
+import datetime
 
 class Leveraged(IStrategy):
-    """
 
-    author@: Gert Wohlgemuth
+    minimal_roi = { "0": 0.25 }
 
-    works on new objectify branch!
-
-    idea:
-        only buy on an upward tending market
-    """
-
-    # Minimal ROI designed for the strategy.
-    # This attribute will be overridden if the config file contains "minimal_roi"
-    #minimal_roi = {
-    #    "0": 0.01
-    #}
-    minimal_roi = {
-        "120": 0.0,
-        "45" : 0.01,
-        "30" : 0.015,
-        "0" : 0.025
-    }
-
-    # Optimal stoploss designed for the strategy
-    # This attribute will be overridden if the config file contains "stoploss"
     stoploss = -0.05
+    
+    trailing_stop = True
 
-    # Optimal timeframe for the strategy
-    timeframe = '5m'
+    sell_profit_only=True
 
-    # resample factor to establish our general trend. Basically don't buy if a trend is not given
-    resample_factor = 12
+    timeframe = '1m'
+    
+## INIZIO gestione dual timing
+    informative_timeframe = '15m'
 
-    EMA_SHORT_TERM = 5
-    EMA_MEDIUM_TERM = 12
-    EMA_LONG_TERM = 21
+    def informative_pairs(self):
+        pairs = self.dp.current_whitelist()
+        informative_pairs = [(pair, self.informative_timeframe) for pair in pairs]
+        return informative_pairs
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe = self.resample(dataframe, self.timeframe, self.resample_factor)
 
-        ##################################################################################
-        # buy and sell indicators
+        if self.config['runmode'].value in ('backtest', 'hyperopt'):
+            assert (timeframe_to_minutes(self.timeframe) <= 5), "Backtest this strategy in 5m or 1m timeframe."
 
-        dataframe['ema_{}'.format(self.EMA_SHORT_TERM)] = ta.EMA(
-            dataframe, timeperiod=self.EMA_SHORT_TERM
-        )
-        dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)] = ta.EMA(
-            dataframe, timeperiod=self.EMA_MEDIUM_TERM
-        )
-        dataframe['ema_{}'.format(self.EMA_LONG_TERM)] = ta.EMA(
-            dataframe, timeperiod=self.EMA_LONG_TERM
-        )
+        if self.timeframe == self.informative_timeframe:
+            dataframe = self.do_indicators(dataframe, metadata)
+        else:
+            if not self.dp:
+                return dataframe
 
-        bollinger = qtpylib.bollinger_bands(
-            qtpylib.typical_price(dataframe), window=20, stds=2
-        )
-        dataframe['bb_lowerband'] = bollinger['lower']
-        dataframe['bb_middleband'] = bollinger['mid']
-        dataframe['bb_upperband'] = bollinger['upper']
+            informative = self.dp.get_pair_dataframe(pair=metadata['pair'], timeframe=self.informative_timeframe)
 
-        dataframe['min'] = ta.MIN(dataframe, timeperiod=self.EMA_MEDIUM_TERM)
-        dataframe['max'] = ta.MAX(dataframe, timeperiod=self.EMA_MEDIUM_TERM)
+            informative = self.do_indicators(informative.copy(), metadata)
 
-        dataframe['cci'] = ta.CCI(dataframe)
-        dataframe['mfi'] = ta.MFI(dataframe)
-        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=7)
+            dataframe = merge_informative_pair(dataframe, informative, self.timeframe, self.informative_timeframe, ffill=True)
+            skip_columns = [(s + "_" + self.informative_timeframe) for s in ['date', 'open', 'high', 'low', 'close', 'volume']]
+            dataframe.rename(columns=lambda s: s.replace("_{}".format(self.informative_timeframe), "") if (not s in skip_columns) else s, inplace=True)
 
-        dataframe['average'] = (dataframe['close'] + dataframe['open'] + dataframe['high'] + dataframe['low']) / 4
+        return dataframe
+## FINE gestione dual timing
 
-        ##################################################################################
-        # required for graphing
-        bollinger = qtpylib.bollinger_bands(dataframe['close'], window=20, stds=2)
-        dataframe['bb_lowerband'] = bollinger['lower']
-        dataframe['bb_upperband'] = bollinger['upper']
-        dataframe['bb_middleband'] = bollinger['mid']
+## Trailing stoploss with positive offset
+    use_custom_stoploss = True
 
+    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
+                        current_rate: float, current_profit: float, **kwargs) -> float:
+
+        if current_profit < 0.005:
+            return -1 # return a value bigger than the initial stoploss to keep using the initial stoploss
+
+        # After reaching the desired offset, allow the stoploss to trail by half the profit
+        desired_stoploss = current_profit / 2
+
+        # Use a minimum of 2.5% and a maximum of 5%
+        return max(min(desired_stoploss, 0.10), 0.015)
+    
+### FINE  Trailing stoploss with positive offset
+
+    def do_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        
         macd = ta.MACD(dataframe)
         dataframe['macd'] = macd['macd']
         dataframe['macdsignal'] = macd['macdsignal']
         dataframe['macdhist'] = macd['macdhist']
+        dataframe['cci'] = ta.CCI(dataframe)
 
         return dataframe
+        
+#        dataframe['ema3'] = ta.EMA(dataframe, timeperiod=3)
+#        dataframe['ema5'] = ta.EMA(dataframe, timeperiod=5)
+#        dataframe['go_long'] = qtpylib.crossed_above(dataframe['ema3'], dataframe['ema5']).astype('int')
+#        dataframe['go_short'] = qtpylib.crossed_below(dataframe['ema3'], dataframe['ema5']).astype('int')
+#        return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        """
-        Based on TA indicators, populates the buy signal for the given dataframe
-        :param dataframe: DataFrame
-        :return: DataFrame with buy column
-        """
         dataframe.loc[
             (
-                    (
-                            (
-                                    (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
-                                    (dataframe['close'] < dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
-                                    (dataframe['close'] == dataframe['min']) &
-                                    (dataframe['close'] <= dataframe['bb_lowerband'])
-                            )
-                            |
-                            # simple v bottom shape (lopsided to the left to increase reactivity)
-                            # which has to be below a very slow average
-                            # this pattern only catches a few, but normally very good buy points
-                            (
-                                    (dataframe['average'].shift(5) > dataframe['average'].shift(4))
-                                    & (dataframe['average'].shift(4) > dataframe['average'].shift(3))
-                                    & (dataframe['average'].shift(3) > dataframe['average'].shift(2))
-                                    & (dataframe['average'].shift(2) > dataframe['average'].shift(1))
-                                    & (dataframe['average'].shift(1) < dataframe['average'].shift(0))
-                                    & (dataframe['low'].shift(1) < dataframe['bb_middleband'])
-                                    & (dataframe['cci'].shift(1) < -100)
-                                    & (dataframe['rsi'].shift(1) < 30)
-                                    & (dataframe['mfi'].shift(1) < 30)
-
-                            )
-                            |
-                            # buy in very oversold conditions
-                            (
-                                    (dataframe['low'] < dataframe['bb_middleband'])
-                                    & (dataframe['cci'] < -200)
-                                    & (dataframe['rsi'] < 30)
-                                    & (dataframe['mfi'] < 30)
-                            )
-
-                    )
-                    # safeguard against down trending markets and a pump and dump
-                    &
-                    (
-                            (dataframe['volume'] < (dataframe['volume'].rolling(window=30).mean().shift(1) * 20)) &
-                            (dataframe['resample_sma'] < dataframe['close']) &
-                            (dataframe['resample_sma'].shift(1) < dataframe['resample_sma'])
-                    )
-            )
-            ,
+                qtpylib.crossed_above(dataframe['macd'], dataframe['macdsignal']) 
+                #& (dataframe['cci'] <= -50.0)
+            ),
             'buy'] = 1
 
         return dataframe
 
+#        dataframe.loc[
+#            qtpylib.crossed_above(dataframe['go_long'], 0)
+#        ,
+#        'buy'] = 1
+#
+#        return dataframe
+
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        """
-        Based on TA indicators, populates the sell signal for the given dataframe
-        :param dataframe: DataFrame
-        :return: DataFrame with buy column
-        """
         dataframe.loc[
             (
-                    (dataframe['close'] > dataframe['ema_{}'.format(self.EMA_SHORT_TERM)]) &
-                    (dataframe['close'] > dataframe['ema_{}'.format(self.EMA_MEDIUM_TERM)]) &
-                    (dataframe['close'] >= dataframe['max']) &
-                    (dataframe['close'] >= dataframe['bb_upperband']) &
-                    (dataframe['mfi'] > 80)
-            ) |
+                qtpylib.crossed_below(dataframe['macd'], dataframe['macdsignal']) 
+                #& (dataframe['cci'] >= 100.0)
+            ),
+            'sell'] = 1
 
-            # always sell on eight green candles
-            # with a high rsi
-            (
-                    (dataframe['open'] < dataframe['close']) &
-                    (dataframe['open'].shift(1) < dataframe['close'].shift(1)) &
-                    (dataframe['open'].shift(2) < dataframe['close'].shift(2)) &
-                    (dataframe['open'].shift(3) < dataframe['close'].shift(3)) &
-                    (dataframe['open'].shift(4) < dataframe['close'].shift(4)) &
-                    (dataframe['open'].shift(5) < dataframe['close'].shift(5)) &
-                    (dataframe['open'].shift(6) < dataframe['close'].shift(6)) &
-                    (dataframe['open'].shift(7) < dataframe['close'].shift(7)) &
-                    (dataframe['rsi'] > 70)
-            )
-                    |
-                    # in case of very overbought market, like some one pumping
-                    # sell
-                    (
-                        (dataframe['cci'] > 200)
-                        & (dataframe['rsi'] > 70)
-                    )
-            ,
-            'sell'
-        ] = 1
         return dataframe
 
-    def resample(self, dataframe, interval, factor):
-        # defines the reinforcement logic
-        # resampled dataframe to establish if we are in an uptrend, downtrend or sideways trend
-        df = dataframe.copy()
-        df = df.set_index(DatetimeIndex(df['date']))
-        ohlc_dict = {
-            'open': 'first',
-            'high': 'max',
-            'low': 'min',
-            'close': 'last'
-        }
-        df = df.resample(str(int(interval[:-1]) * factor) + 'min',
-                         label="right").agg(ohlc_dict).dropna(how='any')
-        df['resample_sma'] = ta.SMA(df, timeperiod=25, price='close')
-        df = df.drop(columns=['open', 'high', 'low', 'close'])
-        df = df.resample(interval[:-1] + 'min')
-        df = df.interpolate(method='time')
-        df['date'] = df.index
-        df.index = range(len(df))
-        dataframe = merge(dataframe, df, on='date', how='left')
-        return dataframe
+#        dataframe.loc[
+#            qtpylib.crossed_above(dataframe['go_short'], 0)
+#        ,
+#        'sell'] = 1
+#
+#        return dataframe
